86759e729c587b544db62f7ad4b969cb176a289a
InigoSanz <i.sanzdelg@gmail.com>
2025-04-09 20:59:37 +0200

    Toma de contacto con la materia

---
+++ Clase 1/index.js
@@ -0,0 +1,10 @@
+let count = 0;
+
+const button = document.getElementById("counter-button");
+const countSpan = document.getElementById("count");
+
+console.log(button);
+
+button.addEventListener("click", () => {});
+
+countSpan.innerText = count;
