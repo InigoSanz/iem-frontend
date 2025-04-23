b2b59e0e73603a82dcea6efff25e2e0dcfc64fa7
InigoSanz <i.sanzdelg@gmail.com>
2025-04-23 16:55:47 +0200

    Mas JS

--- Clase 3/index.js
+++ Clase 3/index.js
@@ -7,3 +7,39 @@ const c = 2;
 a = 2;
 b = 3;
 //c = 4;
+
+var e = 6;
+
+function foo() {
+  //let d = 5;
+
+  console.log(e);
+}
+
+//console.log(d);
+console.log(e);
+
+let text = "Hello world";
+let age = 27;
+let isActive = true;
+let email;
+let cars = null;
+// Poner los console.log para visualizar...
+
+let x = 1;
+let y = 2;
+console.log(x + y); // = 3
+// Más ejemplos con operadores...
+
+// Comparaciones, se puede comparar por valor
+// y también por valor y tipo
+let l = 3;
+let m = "3";
+console.log(l == m); // = true
+console.log(l === m); // = false
+
+const hasCar = true;
+const hasBike = false;
+console.log(hasCar && hasBike); // false
+console.log(hasCar || hasBike); // true
+console.log(!hasCar); // false
