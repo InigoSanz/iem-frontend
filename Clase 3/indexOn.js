f6191f27b64a5a6cfd14814521d55bd2a6dbae93
InigoSanz <i.sanzdelg@gmail.com>
2025-04-23 19:16:18 +0200

    Empezamos con el DOM en JS

---
+++ Clase 3/indexOn.js
@@ -0,0 +1,8 @@
+//console.log("Hola mundo");
+
+const title = document.querySelector("h1");
+console.log(title);
+
+title.innerText = "New Title";
+title.innerHTML = "<p>New paragraph</p>";
+title.style.color = "red";
