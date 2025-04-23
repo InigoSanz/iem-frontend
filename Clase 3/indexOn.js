a182d092d3503075f9dbbbbde5a007c9dff8bccf
InigoSanz <i.sanzdelg@gmail.com>
2025-04-23 19:31:36 +0200

    Más ejemplos DOM

--- Clase 3/indexOn.js
+++ Clase 3/indexOn.js
@@ -5,4 +5,22 @@ console.log(title);
 
 title.innerText = "New Title";
 title.innerHTML = "<p>New paragraph</p>";
-title.style.color = "red";
+//title.style.color = "red";
+
+title.classList.add("title");
+
+const newElement = document.createElement("div");
+newElement.innerText = "Div creado por JS";
+
+const body = document.querySelector("body");
+body.appendChild(newElement);
+
+const button = document.querySelector("button");
+button.addEventListener("click", (event) => {
+  console.log("Has hecho click");
+});
+
+// Hay diversos eventos, por ejemplo "mouseover"
+
+// Si metemos todo el anterior codigo dentro, conseguimos que el codigo se ejecute solo cuando todo el HTML se haya cargado
+// document.addEventListener("DOMContentLoaded", () => {});
