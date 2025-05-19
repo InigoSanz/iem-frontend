d7e8c151de8365d335b8752ecf624b96cccc9887
InigoSanz <i.sanzdelg@gmail.com>
2025-05-19 17:07:27 +0200

    Update Character Detail Component TS
    Cambio de la lógica a como se hacerlo yo

--- front-end-module-iem/src/app/modules/characters/pages/character-detail/character-detail.component.ts
+++ front-end-module-iem/src/app/modules/characters/pages/character-detail/character-detail.component.ts
@@ -3,7 +3,6 @@ import { Character } from "../../models/character.model";
 import { CommonModule } from "@angular/common";
 import { CharacterService } from "../../../../core/services/entity/character.service";
 import { ActivatedRoute } from "@angular/router";
-import { switchMap } from "rxjs";
 import { __param } from "tslib";
 
 @Component({
@@ -14,27 +13,30 @@ import { __param } from "tslib";
 })
 export class CharacterDetailComponent implements OnInit {
   public character: Character | null = null;
-
+  private numberId: number = 0;
   constructor(
     private _characterService: CharacterService,
     private _route: ActivatedRoute
   ) {}
 
   ngOnInit(): void {
-    this._route.paramMap
-      .pipe(
-        switchMap((params) => {
-          const id = params.get("id");
-          return id ? this._characterService.getById(+id) : [];
-        })
-      )
-      .subscribe({
-        next: (character) => {
-          this.character = character;
-        },
-        error: (err) => {
-          console.error("Error al cargar el personaje");
-        },
-      });
+    this._route.paramMap.subscribe((params) => {
+      // Se suscribe a la ruta con el parametro el cual es un ID, este emite los parametros de la url
+      const id = params.get("id");
+      if (id) {
+        // verifica que el id no es nulo
+        this.numberId = parseInt(id);
+        this._characterService.getById(this.numberId).subscribe({
+          // +id lo que hace es convertir de string a numero, luego nos suscribimos
+          next: (character) => {
+            // es la respuesta recibida con los datos
+            this.character = character; // guardamos en nuestra variable el personaje recibido
+          },
+          error: (err) => {
+            console.error("Error al cargar el personaje:", err);
+          },
+        });
+      }
+    });
   }
 }
