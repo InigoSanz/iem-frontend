0d15ded8aa0fd9da84e2ec1dfe23cd7a4bf157a8
InigoSanz <i.sanzdelg@gmail.com>
2025-05-21 16:57:37 +0200

    Implementamos el snackbar en el logout

--- front-end-module-iem/src/app/shared/header/header.component.ts
+++ front-end-module-iem/src/app/shared/header/header.component.ts
@@ -2,6 +2,7 @@ import { CommonModule } from "@angular/common";
 import { Component } from "@angular/core";
 import { RouterModule, Router } from "@angular/router";
 import { AuthService } from "../../core/services/auth/auth.service";
+import { SnackBarService } from "../../core/services/snackbar/snackbar.service";
 
 @Component({
   selector: "app-header",
@@ -12,7 +13,11 @@ import { AuthService } from "../../core/services/auth/auth.service";
 export class HeaderComponent {
   protected isLoggedIn = false;
 
-  constructor(private _authService: AuthService, private _router: Router) {
+  constructor(
+    private _authService: AuthService,
+    private _router: Router,
+    private _snackBarService: SnackBarService
+  ) {
     this._authService.isLoggedIn$.subscribe((isLoggedIn: boolean) => {
       this.isLoggedIn = isLoggedIn;
     });
@@ -21,5 +26,6 @@ export class HeaderComponent {
   logout(): void {
     this._authService.logout();
     this._router.navigate(["/login"]);
+    this._snackBarService.showSnackBar("Sesión cerrada correctamente");
   }
 }
